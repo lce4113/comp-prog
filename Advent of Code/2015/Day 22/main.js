c760aaccde31176ae68d8b70f63130494788ec31
Om Mahesh <2om.mahesh@gmail.com>
2025-12-10 07:41:08 -0800

    remove unnecessary files

--- Advent of Code/2015/Day 22/main.js
+++
@@ -1,7 +0,0 @@
-hp = 50
-mana = 500
-
-ehp = 71
-edmg = 10
-
-b = a.split('\n').map(x => x.match(/\d+/g).map(Number))
--- Advent of Code/2021/Day 2/main2.py
+++
@@ -1,7 +0,0 @@
-from sys import stdin
-
-a = []
-for line in stdin:
-    a.append(int(line))
-
-print(a[3])
